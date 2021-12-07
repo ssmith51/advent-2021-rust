ce548ad7094205266f31eb07629ada6b342a8201
Spencer Smith <spencer.smith.2009@gmail.com>
2021-12-06 23:29:00 -0500

    Puzzle 2...

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -15,8 +15,8 @@ fn main() {
   
   println!("----------------------");
   println!("Starting Puzzle 2");
-  let power = puzzle_2(&report);
-  println!("Power Consumption: {}", power);
+  let power = puzzle_2();
+  println!("Power Consumption: {}", power.unwrap());
 
 }
 
@@ -76,44 +76,28 @@ fn puzzle_1(report: &Vec<String>) -> i64 {
  
 }
 
+//WTF is going on here :) 
+fn puzzle_2() -> Result<u32, Box<dyn std::error::Error>> {
 
-fn puzzle_2(report: &Vec<String>) -> i64 {
+  //Read in the test file from the root dir as a string
+  let report = include_str!("../test.txt");
 
-  let (mut pos, mut one_count, mut zero_count, mut gamma, mut eplison): (usize, i64, i64, String, String) = (0, 0, 0,"".to_string(),"".to_string());
-
-  while pos < report[0].len() {
-
-    for line in report {
-
-      let c = line.chars().nth(pos).unwrap();
-      if c == '1' {
-        zero_count = zero_count + 1;
-      } else if c == '0' {
-        one_count = one_count + 1;
-      }
+  //I still need to understand how this works better... took it from https://github.com/wfxr/advent-of-code-2021/blob/main/src/day03/mod.rs 
+  //to learn different way sto program in Rust. 
+  let rating = |most_common: bool| -> Result<u32, Box<dyn std::error::Error>> {
+    let mut seq: Vec<_> = report.lines().collect();
+    let mut col = 0;
+    while seq.len() > 1 {
+        let ones = seq.iter().filter(|l| l.as_bytes()[col] == b'1').count();
+        let bit = match (most_common, ones * 2 >= seq.len()) {
+            (true, true) | (false, false) => b'1',
+            _ => b'0',
+        };
+        seq = seq.into_iter().filter(|l| l.as_bytes()[col] == bit).collect();
+        col += 1;
     }
-
-    if zero_count > one_count {
-      gamma.push('0');
-      eplison.push('1');
-    } else {
-      gamma.push('1');
-      eplison.push('0');
-    }
-
-    zero_count = 0;
-    one_count = 0;
-    pos = pos + 1;
-  }
-
-  println!("Gamma Binary: {}", gamma);
-  println!("Eplison Binary: {}", eplison);
-
-  //Convert gamma & eplison from String to str and parse to i64s
-  let gamma: i64 = i64::from_str_radix(&gamma[..], 2).unwrap();
-  let eplison: i64 = i64::from_str_radix(&eplison[..], 2).unwrap();
-
-  let power: i64 =  gamma * eplison;
-  power
- 
+    Ok(u32::from_str_radix(seq.first().ok_or("empty input")?, 2)?)
+  };
+  let (oxygen, co2) = (rating(true)?, rating(false)?);
+  Ok(oxygen * co2)
 }
