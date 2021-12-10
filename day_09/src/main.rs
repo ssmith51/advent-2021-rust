8d0a98e41329b6ed1d263647777a44bcd5413ce0
Spencer Smith <spencer.smith.2009@gmail.com>
2021-12-09 22:38:17 -0500

    Day 09 Puzzle 01

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 //Gobal file name for quick change
-static FILE_NAME: &str = "test.txt";
+static FILE_NAME: &str = "input.txt";
 
 struct HeightMap {
   readings: Vec<Vec<u32>>,
@@ -13,7 +13,14 @@ struct HeightMap {
 fn main() {
   println!("Advent of Code - Day 09");
   let height_map = read_input(FILE_NAME);
-  println!("Max X: {}, Max Y: {}", height_map.max_x, height_map.max_y);
+  println!("----------------------");
+  println!("Starting Puzzle 1");
+  let result = puzzle_1(&height_map);
+  println!("Sum of Risk Levels: {}", result);
+  println!("----------------------");
+  println!("Starting Puzzle 2");
+  let result = puzzle_2(&height_map);
+  println!("Three Largest Basins: {}", result);
   
 }
 
@@ -43,8 +50,74 @@ fn read_input(filename: &str) -> HeightMap {
   }
 
   //Set the X & Y values for the grid
-  height_map.max_x = height_map.readings[0].len();
-  height_map.max_y = height_map.readings.len();
+  height_map.max_x = height_map.readings[0].len() -1;
+  height_map.max_y = height_map.readings.len() -1 ;
 
   height_map
+}
+
+fn puzzle_1(height_map: &HeightMap) -> i32 {
+  let mut sum: i32 = 0;
+
+  let (mut y, mut x): (usize, usize) = (0,0);
+
+  for row in &height_map.readings {
+
+    for val in row {
+      
+      //Calculate comparision indexes
+      let bottom :i32 = if y == height_map.max_y {10} else {(height_map.readings[(y+1)][x])as i32};
+      let top: i32 = if y == 0 {10} else {height_map.readings[(y-1)][x] as i32};
+      let prev: i32 = if x == 0 {10} else {row[(x-1)] as i32};
+      let next :i32 = if x < height_map.max_x {row[(x+1)] as i32} else {10};
+
+      let val = *val as i32;
+
+      if val < prev && val < next && val < top && val < bottom {
+        sum += val+1;
+      }
+      x += 1;
+    } 
+
+    //Rest X & Increment Y
+    x = 0; 
+    y += 1;
+  }
+
+  sum 
+}
+
+fn puzzle_2(height_map: &HeightMap) -> i32 {
+  let mut sum: i32 = 0;
+
+  let (mut y, mut x): (usize, usize) = (0,0);
+
+  for row in &height_map.readings {
+
+    for val in row {
+      
+      //Calculate comparision indexes
+      let bottom :i32 = if y == height_map.max_y {10} else {(height_map.readings[(y+1)][x])as i32};
+      let top: i32 = if y == 0 {10} else {height_map.readings[(y-1)][x] as i32};
+      let prev: i32 = if x == 0 {10} else {row[(x-1)] as i32};
+      let next :i32 = if x < height_map.max_x {row[(x+1)] as i32} else {10};
+
+      let val = *val as i32;
+
+      if val < prev && val < next && val < top && val < bottom {
+
+        //Need to do some type of basic graph searching here, keeping track of the total size
+        //Maybe use a recurisve function to constantly shift the grids and track the basin...
+
+        sum += val+1;
+      }
+      x += 1;
+    } 
+
+    //Rest X & Increment Y
+    x = 0; 
+    y += 1;
+  }
+
+  sum 
 }
