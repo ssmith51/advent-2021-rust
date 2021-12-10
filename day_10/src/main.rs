ac9c2a943ca02978ef0f72181094e3d96d1611d0
Spencer Smith <spencer.smith.2009@gmail.com>
2021-12-10 02:48:05 -0500

    Puzzle 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+
 // use regex::Regex;
 
 //Gobal file name for quick change
@@ -11,6 +12,10 @@ fn main() {
     println!("----------------------");
     println!("Starting Puzzle 1");
     let result = puzzle_1(&input);
+    println!("Final Score: {}", result);
+    println!("----------------------");
+    println!("Starting Puzzle 2");
+    let result = puzzle_2(&input);
     println!("Final Score: {}", result);
 
 }
@@ -56,10 +61,9 @@ fn puzzle_1(input: &Vec<String>) -> i64 {
         expected.pop();
       }
 
-      
-
     }
 
+    //Calculate Illegal Score
     if illegal == ')' {
       score += 3; 
     } else if illegal == ']' {
@@ -70,96 +74,81 @@ fn puzzle_1(input: &Vec<String>) -> i64 {
       score += 25137;
     }
 
-
-
-
-
-    // let mut test: String = line.clone();
-
-    // let mut matched = re.is_match(&line[..]);
-
-    // let mut removed: i64 = 0;
-    // let mut illegal_char: char = '1';
-
-    // while matched {
-
-    //   let capture = re.captures(&test).unwrap().get(1).unwrap().as_str();
-    //   let caps: Vec<char> = capture.chars().collect();
-    //   let mut temp: String = "".to_string();
-
-    //   for (_i, c) in line.chars().enumerate() {
-
-    //     if c != caps[0] && c != caps[1] && removed < 2{
-    //       illegal_char = c;
-    //       temp.push(c);
-    //       removed += 1;
-    //     } 
-    //   }
-
-    //   matched = re.is_match(&temp[..]);
-    //   test = temp;
-
-    //   println!("Test String: {}", test);
-
-    // }
-
-    // if test.len() > 0 {
-    //   let c: Vec<char> = test.chars().collect();
-    //   if c[0] != c[1]{
-    //     println!("Final: {}", illegal_char);
-    //     if illegal_char == ')' {
-    //       score += 3; 
-    //     } else if illegal_char == ']' {
-    //       score += 57;
-    //     } else if illegal_char == '}' {
-    //       score += 1197;
-    //     } else if illegal_char == '>' {
-    //       score += 25137;
-    //     }
-    //   }
-    // }
   }
-  
-  // let mut test: String = "[<>({}){}[([)]<>]]".to_string();
+
+
+  score
+
+}
+
+
+fn puzzle_2(input: &Vec<String>) -> i64 {
+  let mut score: i64 = 0;
+  let mut scores: Vec<i64> = Vec::new();
+
+  for line in input {
+
+    let mut expected: Vec<char> = Vec::new();
+    for (_index, c) in  line.chars().enumerate() {
+
+      if c == '(' {
+        expected.push(')');
+      } else if c == '[' {
+        expected.push(']');
+
+      } else if c == '{' {
+        expected.push('}');
+
+      } else if c == '<' {
+       expected.push('>');
+
+      } else if &c != expected.get(expected.len() - 1).unwrap() {
+        expected.clear();
+        break;
+      } else if &c == expected.last().unwrap() {
+        expected.pop();
+      }
+
+    }
+
+    
+
+    //Find the remaining scores
+    if expected.len() > 0 {
+
+      let mut i: i32 = (expected.len() -1) as i32; 
+
+      while i >= 0 {
+
+        score = score * 5;
+
+        if expected.get(i as usize).unwrap() == &')' {
+          score += 1; 
+        } else if expected.get(i as usize).unwrap() == &']' {
+          score += 2;
+        } else if expected.get(i as usize).unwrap() == &'}' {
+          score += 3;
+        } else if expected.get(i as usize).unwrap() == &'>' {
+          score += 4;
+        }
+
+        i -= 1; 
+      }
+
+      scores.push(score);
+      score = 0;
+
+    }
 
   
-  // let mut matched = re.is_match(&test[..]);
-  // println!("Matched: {}",re.is_match(&test[..]));
-  // let mut removed: i64 = 0;
-  // let mut illegal_char: char = '1';
-  // while matched {
 
-  //   let capture = re.captures(&test).unwrap().get(1).unwrap().as_str();
-  //   let caps: Vec<char> = capture.chars().collect();
-  //   let mut temp: String = "".to_string();
-  //   for (_i, c) in test.chars().enumerate() {
+  }
 
-  //     if c != caps[0] && c != caps[1] && removed < 2{
-  //       temp.push(c);
-  //       removed += 1;
-  //     } else if removed == 2 {
-  //       illegal_char = c;
-  //     }
-  //   }
-
-  //   test = temp;
-  //   // println!("Test: {}", test);
-  //   matched = re.is_match(&test[..]);
-  //   // println!("Re-Matched: {}", matched);
-  // }
-
-  // if test.len() > 0 {
-  //   println!("Final: {}", illegal_char);
-  //   if illegal_char == ')' {
-  //     score += 3; 
-  //   } else if illegal_char == ']' {
-  //     score += 57;
-  //   } else if illegal_char == '}' {
-  //     score += 1197;
-  //   } else if illegal_char == '>' {
-  //     score += 25137;
-  //   }
-  // }
+  scores.sort();
+  println!("Number of Scores: {}", scores.len());
+  let median = scores.len() / 2;
+  println!("Median Score Position: {}", median);
+  score = *scores.get(median as usize).unwrap();
 
   score
 
