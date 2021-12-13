77ab5abaca6b78427a808d46c330cb02997fb151
Spencer Smith <ssmith2@netjets.com>
2021-12-13 17:29:52 -0500

    Day 12 Updates

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -5,12 +5,12 @@ use std::collections::HashMap;
 //Gobal file name for quick change
 const FILE_NAME: &str = "small-test.txt";
 
-#[derive(Debug)]
-struct Cave {
-  name: String, 
-  is_small: bool, 
-  connected: Vec<String>, //Need to convert this to a Vec<Cave>
-}
+// #[derive(Debug)]
+// struct Cave {
+//   name: String, 
+//   is_small: bool, 
+//   connected: Vec<String>, //Need to convert this to a Vec<Cave>
+// }
 
 fn main() {
   println!("Advent of Code - Day 12");
@@ -19,9 +19,9 @@ fn main() {
   puzzle_1(caves);
 }
 
-fn read_input(filename: &str) -> HashMap<String, Cave>{
+fn read_input(filename: &str) -> HashMap<String, Vec<String>>{
 
-  let mut caves: HashMap<String, Cave> = HashMap::new();
+  let mut caves: HashMap<String, Vec<String>> = HashMap::new();
 
   let fi = File::open(filename).unwrap();
   let reader = BufReader::new(fi);
@@ -30,25 +30,33 @@ fn read_input(filename: &str) -> HashMap<String, Cave>{
     let line = line.unwrap();
 
     let parts: Vec<&str> = line.split("-").collect();
-    let name = parts[0].to_string();
-    let existing_cave = caves.get_mut(&name);
+    let cave_1 = parts[0].to_string();
+    let cave_2 = parts[1].to_string();
+    
+    //Try building a hashmap of caves and their connected caves
+    caves.entry(cave_1.clone()) 
+      .and_modify(|neighbor| neighbor.push(cave_2.clone()))
+      .or_insert(vec![cave_2.clone()]);
 
-git 
-    if existing_cave.is_none() { //If the cave does not exist, create a new one
-      let is_small = name.chars().all(|c| c.is_ascii_lowercase()); //check if all the chars are lowercase
-      let mut connected: Vec<String> = Vec::new();
-      connected.push(parts[1].to_string());
+    caves.entry(cave_2)
+      .and_modify(|neighbor| neighbor.push(cave_1.clone()))
+      .or_insert(vec![cave_1]);
+
+    // if existing_cave.is_none() { //If the cave does not exist, create a new one
+    //   let is_small = name.chars().all(|c| c.is_ascii_lowercase()); //check if all the chars are lowercase
+    //   let mut connected: Vec<String> = Vec::new();
+    //   connected.push(parts[1].to_string());
   
-        //Create a 'Cave'
-      let c: Cave = Cave {
-        name: name, 
-        is_small: is_small,
-        connected: connected,
-      };
-      caves.insert(parts[0].to_string(), c);
-    } else { // If the cave does exist, add a new cave
-      existing_cave.unwrap().connected.push(parts[1].to_string());
-    }
+    //     //Create a 'Cave'
+    //   let c: Cave = Cave {
+    //     name: name, 
+    //     is_small: is_small,
+    //     connected: connected,
+    //   };
+    //   caves.insert(parts[0].to_string(), c);
+    // } else { // If the cave does exist, add a new cave
+    //   existing_cave.unwrap().connected.push(parts[1].to_string());
+    // }
   }
 
   caves
@@ -56,9 +64,14 @@ git
 }
 
 
-fn puzzle_1(caves: HashMap<String, Cave> ) {
+fn puzzle_1(caves: HashMap<String, Vec<String>> ) {
   //Find all paths from Start to End
   //Remove any paths that go through small twice 
+
+  let mut paths: Vec<String> = Vec::new();
   let start = caves.get("start").unwrap();
+  start.iter().map(|cave| paths.push(cave.clone()));
+
+  println!("Start Cave: {:?}", start);
 
 }
