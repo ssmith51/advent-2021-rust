ac51869f86855a6bc81a665ed7261fc6e81ca8a5
Spencer Smith <ssmith2@netjets.com>
2021-12-20 14:23:26 -0500

    Day 15 working with recursion... on the test input

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -4,7 +4,7 @@ use std::time::{Instant};
 use std::cmp::{min};
 
 //Gobal file name for quick change
-const FILE_NAME: &str = "test.txt";
+const FILE_NAME: &str = "input.txt";
 
 fn main() {
   println!("Advent of Code - Day 12");
@@ -22,44 +22,48 @@ fn main() {
 }
 
 
-fn read_input(filename: &str) -> Vec<Vec<i8>>  {
+fn read_input(filename: &str) -> Vec<Vec<i64>>  {
 
   let fi = File::open(filename).unwrap();
   let reader = BufReader::new(fi);
 
   // New way of reading in input all at once
-  let input : Vec<Vec<i8>> = reader 
+  let input : Vec<Vec<i64>> = reader 
     .lines() // Read each line
     .map(|line| { //For each line, create a map that takes the line and converst it to a vec
       line.unwrap()
       .chars()
-        .map(|c| c.to_digit(10).unwrap() as i8) // Convert each char in vec into an i8 
-        .collect() // Collect all the i8s into an vec
+        .map(|c| c.to_digit(10).unwrap() as i64) // Convert each char in vec into an i64 
+        .collect() // Collect all the i64s into an vec
     })
     .collect(); //Collect all the vecs into a vec
 
   input
 }
 
-fn puzzle_1(grid: Vec<Vec<i8>>) -> i8 {
-  println!("Grid: {:?}", grid);
-  let max_y = grid.len();
-  let max_x = grid[0].len();
+fn puzzle_1(grid: Vec<Vec<i64>>) -> i64 {
+  // println!("Grid: {:?}", grid);
+  let max_y = grid.len() as i64;
+  let max_x = grid[0].len() as i64;
 
-  let total = calc_cost(&grid, max_y, max_x);
+  println!("Calculating Cost");
+  let total = calc_cost(&grid, max_x, max_y);
 
   total
 }
 
-fn calc_cost(grid: &Vec<Vec<i8>>, x: usize, y: usize) -> i8 {
+fn calc_cost(grid: &Vec<Vec<i64>>, x: i64, y: i64) -> i64 {
+  print!(".");
 
-  let mut cost: i8 = 0;
+  let mut cost: i64 = 0;
 
-  if x == 0 && y == 0 {
+  if x == 0 || y == 0 {
+    cost = i64::MAX;
+  } else if x == 1 && y == 1 {
     cost = 0;
+  } else {
+    cost = min(calc_cost(grid, x, y-1), calc_cost(grid, x-1, y)) + grid[(y-1) as usize] [(x-1) as usize];
   }
-
-  
 
   cost
 
